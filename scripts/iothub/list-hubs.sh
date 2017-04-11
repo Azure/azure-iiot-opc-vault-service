10ad22fee66c65cda70ab43068abb9b6d3804ca9
Devis Lucato <devis@lucato.it>
2017-04-11 11:55:52 -0700

    Add scripts to manage IoT Hubs

---
+++ scripts/iothub/list-hubs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
+source "$DIR/.functions.sh"
+
+main() {
+    check_dependencies
+    login
+
+    header "Azure IoT Hubs in the current subscription"
+    az iot hub list | jq '.[] | {name: .name, resourcegroup: .resourcegroup, location: .location, subscription: .subscriptionid}' | jq .
+}
+
+main
---
+++ scripts/iothub/list-subscriptions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
+source "$DIR/.functions.sh"
+
+main() {
+    check_dependencies
+    login
+
+    header "Azure subscriptions"
+    azure account list
+    #az account list | jq '.[] | {name: .name, id: .id, current: .isDefault}' | jq .
+}
+
+main
