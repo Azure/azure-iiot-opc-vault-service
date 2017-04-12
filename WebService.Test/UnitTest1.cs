73a296ea48abd7c7e3990b6a3e4180d1052e7d78
Devis Lucato <devis@lucato.it>
2017-04-12 12:03:57 -0700

    Web service and unit tests project

---
+++ WebService.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Xunit;
+
+namespace WebService.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
