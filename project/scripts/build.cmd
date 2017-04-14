6c9d92faa962c03982f4d7870db53596741528a1
Devis Lucato <delucato@microsoft.com>
2017-04-14 15:37:00 -0700

    Build script for dotnet 1.0.1

--- project/scripts/build.cmd
+++ project/scripts/build.cmd
@@ -1,6 +1,8 @@
 @ECHO off
 setlocal
 
+:: quiet|minimal|normal|detailed|diagnostic
+SET VERBOSITY=normal
 :: Debug|Release
 SET CONFIGURATION=Release
 
@@ -9,11 +11,13 @@ SET APP_HOME=%~dp0
 SET APP_HOME=%APP_HOME:~0,-17%
 cd %APP_HOME%
 
-call dotnet restore *.sln
-call dotnet build --configuration %CONFIGURATION% *.sln
+for %%i in (*.sln) do (
+  call dotnet restore --verbosity %VERBOSITY% %%~nxi
+  call dotnet build --verbosity %VERBOSITY% --configuration %CONFIGURATION% %%~nxi
+)
 
 for /r %%i in (*.Test.csproj) do (
-  call dotnet test --no-build %%i
+  call dotnet test --verbosity %VERBOSITY% --configuration %CONFIGURATION% --no-build %%i
 )
 
 endlocal
