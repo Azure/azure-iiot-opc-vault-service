05e04b8d8d83c725009610d937199f7de398905a
Martin Regen <mregen@microsoft.com>
2018-06-22 08:22:31 +0200

    refactor cosmosdb models

---
+++ CosmosDB.Models/Models/CertificateRequest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Opc.Ua;
+using System;
+
+namespace Microsoft.Azure.IoTSolutions.GdsVault.CosmosDB.Models
+{
+    [Serializable]
+    public class CertificateRequest
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid RequestId { get; set; }
+        public Guid ApplicationId { get; set; }
+        public int State { get; set; }
+        public NodeId CertificateGroupId { get; set; }
+        public NodeId CertificateTypeId { get; set; }
+        public byte[] CertificateSigningRequest { get; set; }
+        public string SubjectName { get; set; }
+        public string[] DomainNames { get; set; }
+        public string PrivateKeyFormat { get; set; }
+        public string PrivateKeyPassword { get; set; }
+        public string AuthorityId { get; set; }
+        public byte[] Certificate { get; set; }
+        public DateTime RequestTime { get; set; }
+        public DateTime ApproveRejectTime { get; set; }
+        public DateTime AcceptTime { get; set; }
+    }
+}
---
+++ CosmosDB.Models/Models/CertificateStore.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Microsoft.Azure.IoTSolutions.GdsVault.CosmosDB.Models
+{
+    [Serializable]
+    public class CertificateStore
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid TrustListId { get; private set; }
+        public string Path { get; set; }
+        public string AuthorityId { get; set; }
+    }
+
+}
