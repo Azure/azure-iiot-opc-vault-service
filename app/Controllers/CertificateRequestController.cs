2295b071bb55fb5fc00b312c5c355fb7df3e418e
Martin Regen <mregen@microsoft.com>
2018-09-07 12:20:11 +0200

    typo

--- app/Controllers/CertificateRequestController.cs
+++ app/Controllers/CertificateRequestController.cs
@@ -7,7 +7,7 @@ using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
-namespace Microsoft.Azure.IIoT.OpcUa.Services.GdsVault.Common.Controllers
+namespace Microsoft.Azure.IIoT.OpcUa.Services.GdsVault.App.Controllers
 {
     [Authorize]
     public class CertificateRequestController : Controller
