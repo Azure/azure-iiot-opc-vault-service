7f929b2839bf2f8a3c5e647bb21848daf14b67aa
Martin Regen <mregen@microsoft.com>
2019-02-07 09:50:25 +0100

    api update (comments)

--- api-csharp/IOpcVault.cs
+++ api-csharp/IOpcVault.cs
@@ -128,14 +128,14 @@ namespace Microsoft.Azure.IIoT.OpcUa.Api.Vault
 
         /// <summary>
         /// Approve or reject a new application.
-        /// &lt;remarks&gt;
+        /// </summary>
+        /// <remarks>
         /// A manager can approve a new application or force an application
         /// from any state.
         /// After approval the application is in the 'Approved' or 'Rejected'
         /// state.
         /// Requires Manager role.
-        /// &lt;/remarks&gt;
-        /// </summary>
+        /// </remarks>
         /// <param name='applicationId'>
         /// The application id
         /// </param>
@@ -518,7 +518,7 @@ namespace Microsoft.Azure.IIoT.OpcUa.Api.Vault
         Task<HttpOperationResponse<string>> CreateSigningRequestWithHttpMessagesAsync(CreateSigningRequestApiModel signingRequest = default(CreateSigningRequestApiModel), Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
-        /// Create a a certificate request with a new key pair.
+        /// Create a certificate request with a new key pair.
         /// </summary>
         /// <remarks>
         /// The request is in the 'New' state after this call.
@@ -540,11 +540,11 @@ namespace Microsoft.Azure.IIoT.OpcUa.Api.Vault
         /// </summary>
         /// <remarks>
         /// Validates the request with the application database.
-        /// If Approved:
-        /// New Key Pair request: Creates the new key pair
+        /// - If Approved:
+        /// - New Key Pair request: Creates the new key pair
         /// in the requested format, signs the certificate and stores the
         /// private key for later securely in KeyVault.
-        /// Cert Signing Request: Creates and signs the certificate.
+        /// - Cert Signing Request: Creates and signs the certificate.
         /// Deletes the CSR from the database.
         /// Stores the signed certificate for later use in the Database.
         /// The request is in the 'Approved' or 'Rejected' state after this
