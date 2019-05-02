70607f4e3e2ecb5611c044443e64213deceb0d8f
Marc Schier <mschier@microsoft.com>
2019-05-02 16:20:56 +0200

    Application sample

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
 FROM microsoft/dotnet:2.2-aspnetcore-runtime AS base
 WORKDIR /app
-EXPOSE 80
+EXPOSE 56310
+EXPOSE 44342
 
 FROM microsoft/dotnet:2.2-sdk AS build
 WORKDIR /src
-COPY src/Microsoft.Azure.IIoT.OpcUa.Services.Vault.csproj src/
-COPY NuGet.Config NuGet.Config
-RUN dotnet restore --configfile NuGet.Config -nowarn:msb3202,nu1503 src/Microsoft.Azure.IIoT.OpcUa.Services.Vault.csproj
-COPY src/ src/
-WORKDIR /src/src
-RUN dotnet build Microsoft.Azure.IIoT.OpcUa.Services.Vault.csproj -c Release -o /app
+COPY app/Microsoft.Azure.IIoT.OpcUa.Services.Vault.App.csproj app/
+COPY api-csharp/Microsoft.Azure.IIoT.OpcUa.Api.Vault.csproj api-csharp/
+RUN dotnet restore app/Microsoft.Azure.IIoT.OpcUa.Services.Vault.App.csproj
+COPY app app/
+COPY api-csharp api-csharp/
+WORKDIR /src/app
+RUN dotnet build Microsoft.Azure.IIoT.OpcUa.Services.Vault.App.csproj -c Release -o /app
 
 FROM build AS publish
-RUN dotnet publish Microsoft.Azure.IIoT.OpcUa.Services.Vault.csproj -c Release -o /app
+RUN dotnet publish Microsoft.Azure.IIoT.OpcUa.Services.Vault.App.csproj -c Release -o /app
 
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app .
-ENTRYPOINT ["dotnet", "Microsoft.Azure.IIoT.OpcUa.Services.Vault.dll"]
+ENTRYPOINT ["dotnet", "Microsoft.Azure.IIoT.OpcUa.Services.Vault.App.dll"]
+
--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,20 @@
-// ------------------------------------------------------------
-//  Copyright (c) Microsoft Corporation.  All rights reserved.
-//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
 
-
+using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.IIoT.OpcUa.Services.Vault.Runtime;
+using Microsoft.Azure.IIoT.OpcUa.Services.Vault.App.Utils;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Events;
-using System;
-using System.Collections.Generic;
-using System.IO;
 
-namespace Microsoft.Azure.IIoT.OpcUa.Services.Vault
+namespace Microsoft.Azure.IIoT.OpcUa.Services.Vault.App
 {
-    /// <summary>Application entry point</summary>
     public class Program
     {
         public static int Main(string[] args)
@@ -26,44 +25,11 @@ namespace Microsoft.Azure.IIoT.OpcUa.Services.Vault
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
                 .CreateLogger();
-
             try
             {
-                /*
-                Kestrel is a cross-platform HTTP server based on libuv, a
-                cross-platform asynchronous I/O library.
-                https://docs.microsoft.com/en-us/aspnet/core/fundamentals/servers
-                */
-                // Load hosting configuration
-                var configRoot = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddCommandLine(args)
-                    .AddEnvironmentVariables("ASPNETCORE_")
-                    .AddJsonFile("hosting.json", true)
-                    .AddInMemoryCollection(new Dictionary<string, string> {
-                    { "urls", "http://*:58801" }
-                    })
-                    .Build();
-
-                /*
-                Print some information to help development and debugging, like
-                runtime and configuration settings
-                */
-                Console.WriteLine($"[{Uptime.ProcessId}] Starting web service, process ID: " + Uptime.ProcessId);
-
-                var host = new WebHostBuilder()
-                    .UseConfiguration(configRoot)
-                    .UseKestrel(options => { options.AddServerHeader = false; })
-                    .UseIISIntegration()
-                    .UseStartup<Startup>()
-                    .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
-                        .ReadFrom.Configuration(hostingContext.Configuration)
-                        .Enrich.FromLogContext()
-                        .WriteTo.Console())
-                    .Build();
-
+                var host = CreateWebHostBuilder(args).Build();
+                Log.Information("Web Host Ready to run.");
                 host.Run();
-
                 return 0;
             }
             catch (Exception ex)
@@ -75,6 +41,48 @@ namespace Microsoft.Azure.IIoT.OpcUa.Services.Vault
             {
                 Log.CloseAndFlush();
             }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var builtConfig = config.Build();
+                    var keyVault = builtConfig["KeyVault"];
+                    if (keyVault != null)
+                    {
+                        var prefix = new PrefixKeyVaultSecretManager("App");
+                        var clientSecret = builtConfig["AzureAD:ClientSecret"];
+                        var clientId = builtConfig["AzureAD:ClientId"];
+                        if (String.IsNullOrWhiteSpace(clientSecret) ||
+                            String.IsNullOrWhiteSpace(clientId))
+                        {
+                            // try managed service identity
+                            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                            config.AddAzureKeyVault(
+                                keyVault,
+                                keyVaultClient,
+                                prefix
+                            );
+                        }
+                        else
+                        {
+                            config.AddAzureKeyVault(
+                                keyVault,
+                                clientId,
+                                clientSecret,
+                                prefix
+                            );
+                        }
+                    }
+                })
+                .UseStartup<Startup>()
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                        .ReadFrom.Configuration(hostingContext.Configuration)
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console());
         }
     }
 }
